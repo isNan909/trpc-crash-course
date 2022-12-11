df2be81f070de815617f293f973e395aff9b4320
ishan-me <ishan02016@gmail.com>
2022-12-11 20:28:18 +0545

    added crud procedure calls

--- src/server/trpc/router/mynotes.ts
+++ src/server/trpc/router/mynotes.ts
@@ -3,6 +3,7 @@ import { z } from "zod";
 import { router, publicProcedure } from "../trpc";
 
 export const notesRouter = router({
+  //fetch all notes
   allNotes: publicProcedure.query(async ({ ctx }) => {
     try {
       return await ctx.prisma?.notes?.findMany({
@@ -18,6 +19,7 @@ export const notesRouter = router({
       console.log(`We cannot fetch your notes ${error}`);
     }
   }),
+  //create a new note
   newNote: publicProcedure
     .input(
       z.object({
@@ -43,4 +45,59 @@ export const notesRouter = router({
         console.log(`New note cannot be creatted ${error}`);
       }
     }),
+  //fetch a single note
+  detailNote: publicProcedure.query(async ({ ctx, input }) => {
+    const { id } = input;
+    try {
+      return await ctx.prisma.notes.findUnique({
+        where: {
+          id: id,
+        },
+      });
+    } catch (error) {
+      console.log(`New note cannot be creatted ${error}`);
+    }
+  }),
+  //update a note
+  updateNote: publicProcedure
+    .input(
+      z.object({
+        title: z
+          .string()
+          .min(5, { message: "Must be 5 or more characters of length!" })
+          .max(200, {
+            message: "Must not be more than 200 characters of length!",
+          })
+          .trim(),
+        description: z.string(),
+      })
+    )
+    .mutation(async ({ ctx, input }) => {
+      try {
+        await ctx.prisma.notes.update({
+          where: {
+            id,
+          },
+          data: {
+            title: input.title,
+            description: input.description,
+          },
+        });
+      } catch (error) {
+        console.log(`New note cannot be updated ${error}`);
+      }
+    }),
+  //delete a note
+  deleteNote: publicProcedure.mutation(async ({ ctx, input }) => {
+    const { id } = input;
+    try {
+      await ctx.prisma.notes.delete({
+        where: {
+          id,
+        },
+      });
+    } catch (error) {
+      console.log(`New note cannot be deleted ${error}`);
+    }
+  }),
 });
